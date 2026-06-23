d38047b32ea72f6abbd43a1a8c76ad999bfe331c
BalandinaM <marina.balandina.89@mail.ru>
2026-06-23 14:27:37 +0500

    feat: добавить эндпоинт для получения деталей фильма

---
+++ kinopoisk/src/app/api/endpoints/movieApi.ts
@@ -0,0 +1,25 @@
+import { withZodCatch } from '@/common/utils'
+import { baseApi } from '../baseApi'
+import type { DetailsParams } from '../types'
+import { ApiEndpoints } from '@/common/constants'
+import { movieDetailsSchema } from '../schemas'
+
+export const movieApi = baseApi.injectEndpoints({
+  endpoints: build => ({
+    fetchMovieDetails: build.query({
+      query: ({
+        movie_id,
+        language = 'ru-RU',
+        append_to_response,
+      }: DetailsParams) => {
+        return {
+          url: `${ApiEndpoints.Movie}/${movie_id}`,
+          params: { language, append_to_response },
+        }
+      },
+      ...withZodCatch(movieDetailsSchema),
+    }),
+  }),
+})
+
+export const { useFetchMovieDetailsQuery } = movieApi
