039987f4bdc608d22ec878d442cecda6e8754356
BalandinaM <marina.balandina.89@mail.ru>
2026-06-24 11:19:55 +0500

    style: поправить верстку карточки фильма

--- kinopoisk/src/common/components/moviesSection/moviePreview/MoviePreview.tsx
+++ kinopoisk/src/common/components/moviesSection/moviePreview/MoviePreview.tsx
@@ -33,26 +33,27 @@ export const MoviePreview = ({ data }: Props) => {
     })
   }
 
-  // if (data) {
   const posterUrl = getPosterUrl(data.poster_path, 'w342')
 
   return (
     <article key={data.id} className={s.card}>
       <Link to={`/movie/${data.id}`} className={s.link}>
-        <img
-          src={posterUrl}
-          alt={data.title}
-          width={180}
-          height={270}
-          className={s.image}
-        />
-        <h4 className={s.title}>{data.title}</h4>
-        <div
-          className={s.rating}
-          data-rating={getRatingColor(data.vote_average)}
-        >
-          {data.vote_average.toFixed(1)}
+        <div className={s.imageWrapper}>
+          <img
+            src={posterUrl}
+            alt={data.title}
+            width={180}
+            height={270}
+            className={s.image}
+          />
+          <div
+            className={s.rating}
+            data-rating={getRatingColor(data.vote_average)}
+          >
+            {data.vote_average.toFixed(1)}
+          </div>
         </div>
+        <h4 className={s.title}>{data.title}</h4>
       </Link>
       <button
         onClick={handleFavoriteClick}
@@ -62,5 +63,4 @@ export const MoviePreview = ({ data }: Props) => {
       </button>
     </article>
   )
-  // }
 }
