71e8b48b59bf0552093c1943088c547b61d64b35
BalandinaM <marina.balandina.89@mail.ru>
2026-06-30 15:59:10 +0500

    feat: добавить пагинацию на страницу поиска, добавить заголовок к инпуту

--- kinopoisk/src/features/search/ui/SearchPage.tsx
+++ kinopoisk/src/features/search/ui/SearchPage.tsx
@@ -3,33 +3,47 @@ import { MoviesSection } from '@/common/components/moviesSection'
 import { useSearchParams } from 'react-router-dom'
 import { SearchInput } from './searchInput'
 import s from './SearchPage.module.css'
+import { useEffect, useState } from 'react'
+import { Pagination } from '@/common/components/pagination/Pagination'
 
 export const SearchPage = () => {
+  const [currentPage, setCurrentPage] = useState(1)
   const [searchParams] = useSearchParams()
   const query = searchParams.get('query')
 
   const { data: searchResult, isLoading } = useSearchMoviesQuery(
-    { query: query || '', page: 1, language: 'ru-RU' },
+    { query: query || '', page: currentPage, language: 'ru-RU' },
     { skip: !query }
   )
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'smooth' })
+  }, [currentPage])
 
   if (query === null || query === '') {
     return (
       <div className={s.wrap}>
-        <SearchInput />
+        <SearchInput setCurrentPage={setCurrentPage} />
       </div>
     )
   }
 
   return (
     <div className={s.wrap}>
-      <SearchInput />
+      <SearchInput setCurrentPage={setCurrentPage} />
       {isLoading && <p>Загрузка...</p>}
       {searchResult?.results.length != 0 ? (
-        <MoviesSection
-          sectionTitle={`Результаты поиска по запросу "${query}"`}
-          movies={searchResult?.results}
-        />
+        <>
+          <MoviesSection
+            sectionTitle={`Результаты поиска по запросу "${query}"`}
+            movies={searchResult?.results}
+          />
+          <Pagination
+            currentPage={currentPage}
+            setCurrentPage={setCurrentPage}
+            pagesCount={searchResult?.total_pages || 1}
+          />
+        </>
       ) : (
         <p>
           {`По вашему запросу "${query}" ни чего не найдено, попробуйте изменить запрос`}
--- kinopoisk/src/features/search/ui/searchInput/SearchInput.tsx
+++ kinopoisk/src/features/search/ui/searchInput/SearchInput.tsx
@@ -3,11 +3,16 @@ import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import s from './SearchInput.module.css'
 
-export const SearchInput = () => {
+type Props = {
+  setCurrentPage: (arg: number) => void
+}
+
+export const SearchInput = ({ setCurrentPage }: Props) => {
   const [search, setSearch] = useState('')
   const navigate = useNavigate()
 
   const handleSearch = () => {
+    setCurrentPage(1)
     if (search.trim()) {
       navigate(`${Path.Search}?query=${search}`)
     }
@@ -15,43 +20,48 @@ export const SearchInput = () => {
 
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'Enter') {
+      setCurrentPage(1)
       handleSearch()
     }
   }
 
   const handleClear = () => {
+    setCurrentPage(1)
     setSearch('')
     navigate(`${Path.Search}`)
   }
 
   return (
-    <div className={s.wrapper}>
-      <input
-        name="search"
-        type="text"
-        className={s.input}
-        value={search}
-        onChange={e => setSearch(e.currentTarget.value)}
-        placeholder={'Поиск фильма по названию...'}
-        onKeyDown={handleKeyDown}
-      />
-      {search && (
+    <div>
+      <h3>Введите название фильма для поиска</h3>
+      <div className={s.wrapper}>
+        <input
+          name="search"
+          type="text"
+          className={s.input}
+          value={search}
+          onChange={e => setSearch(e.currentTarget.value)}
+          placeholder={'Поиск фильма по названию...'}
+          onKeyDown={handleKeyDown}
+        />
+        {search && (
+          <button
+            type="button"
+            className={s.clearButton}
+            onClick={handleClear}
+            aria-label="Очистить"
+          >
+            ✕
+          </button>
+        )}
         <button
-          type="button"
-          className={s.clearButton}
-          onClick={handleClear}
-          aria-label="Очистить"
+          onClick={handleSearch}
+          disabled={!search.trim()}
+          className={s.button}
         >
-          ✕
+          Искать
         </button>
-      )}
-      <button
-        onClick={handleSearch}
-        disabled={!search.trim()}
-        className={s.button}
-      >
-        Искать
-      </button>
+      </div>
     </div>
   )
 }
