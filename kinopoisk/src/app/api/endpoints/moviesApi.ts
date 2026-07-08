e3eae968c85c1b3cc2abc9001b1d4e4521619f5c
BalandinaM <marina.balandina.89@mail.ru>
2026-07-08 10:47:35 +0500

    удалить закомментрованный код

--- kinopoisk/src/app/api/endpoints/moviesApi.ts
+++ kinopoisk/src/app/api/endpoints/moviesApi.ts
@@ -6,42 +6,6 @@ import { ApiEndpoints } from '@/common/constants'
 
 export const moviesApi = baseApi.injectEndpoints({
   endpoints: build => ({
-    // fetchNowPlayingMovies: build.query({
-    //   query: ({ page = 1, language = 'ru-RU' }: MoviesListParams) => {
-    //     return {
-    //       url: ApiEndpoints.NowPlaying,
-    //       params: { page, language },
-    //     }
-    //   },
-    //   ...withZodCatch(moviesResponseSchema),
-    // }),
-    // fetchPopularMovies: build.query({
-    //   query: ({ page = 1, language = 'ru-RU' }: MoviesListParams) => {
-    //     return {
-    //       url: ApiEndpoints.Popular,
-    //       params: { page, language },
-    //     }
-    //   },
-    //   ...withZodCatch(moviesResponseSchema),
-    // }),
-    // fetchTopRatedMovies: build.query({
-    //   query: ({ page = 1, language = 'ru-RU' }: MoviesListParams) => {
-    //     return {
-    //       url: ApiEndpoints.TopRated,
-    //       params: { page, language },
-    //     }
-    //   },
-    //   ...withZodCatch(moviesResponseSchema),
-    // }),
-    // fetchUpcomingMovies: build.query({
-    //   query: ({ page = 1, language = 'ru-RU' }: MoviesListParams) => {
-    //     return {
-    //       url: ApiEndpoints.Upcoming,
-    //       params: { page, language },
-    //     }
-    //   },
-    //   ...withZodCatch(moviesResponseSchema),
-    // }),
     fetchMoviesByCategory: build.query({
       query: ({ category, page = 1, language = 'ru-RU' }: CategoryParams) => {
         return {
@@ -54,10 +18,4 @@ export const moviesApi = baseApi.injectEndpoints({
   }),
 })
 
-export const {
-  // useFetchNowPlayingMoviesQuery,
-  // useFetchPopularMoviesQuery,
-  // useFetchTopRatedMoviesQuery,
-  // useFetchUpcomingMoviesQuery,
-  useFetchMoviesByCategoryQuery,
-} = moviesApi
+export const { useFetchMoviesByCategoryQuery } = moviesApi
