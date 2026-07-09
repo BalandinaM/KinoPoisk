fc5ab549711c896bd562896af80333d75129dac1
BalandinaM <marina.balandina.89@mail.ru>
2026-07-09 23:03:51 +0500

    feat: добавить пагинацию и скелетон

--- kinopoisk/src/features/filteredMovies/ui/FilteredMovies.tsx
+++ kinopoisk/src/features/filteredMovies/ui/FilteredMovies.tsx
@@ -10,11 +10,14 @@ import { RatingSlider } from './ratingSlider'
 import { useDebounceValue } from '@/common/hooks'
 import { SortFilter } from './sortFilter'
 import { GenresFilter } from './genresFilter'
+import { MoviesSectionSkeleton } from '@/common/components/moviesSection/moviesSectionSkeleton'
+import { Pagination } from '@/common/components/pagination/Pagination'
 
 export const FilteredMovies = () => {
   const { data: genresData } = useFetchGenreMovieListQuery({
     language: 'ru-RU',
   })
+  const [currentPage, setCurrentPage] = useState(1)
   const [sort, setSort] = useState<SortOption>(initialStateFilter.sort)
   const [genres, setGenres] = useState<number[]>([])
   const [ratingRange, setRatingRange] = useState<[number, number]>([
@@ -26,12 +29,12 @@ export const FilteredMovies = () => {
   const debounceRating = useDebounceValue(ratingRange)
   const debounceGenres = useDebounceValue(genres)
 
-  const { data: moviesSorted } = useFetchSortedMoviesQuery({
+  const { data: moviesSorted, isLoading } = useFetchSortedMoviesQuery({
     with_genres: debounceGenres.length ? debounceGenres.join(',') : undefined,
     sort_by: debounceSort,
     vote_average_gte: debounceRating[0],
     vote_average_lte: debounceRating[1],
-    page: 1,
+    page: currentPage,
   })
   console.log(genresData)
   console.log(moviesSorted)
@@ -57,7 +60,18 @@ export const FilteredMovies = () => {
         genres={genresData?.genres}
         handleGenreToggle={handleGenreToggle}
       />
-      <MoviesSection movies={moviesSorted?.results} />
+      {isLoading ? (
+        <MoviesSectionSkeleton variant="default" count={20} />
+      ) : (
+        <>
+          <MoviesSection movies={moviesSorted?.results} />
+          <Pagination
+            currentPage={currentPage}
+            setCurrentPage={setCurrentPage}
+            pagesCount={moviesSorted?.total_pages || 1}
+          />
+        </>
+      )}
     </div>
   )
 }
