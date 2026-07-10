f05cd27227d2dc1cfc7f5c9781c2119da47f2da3
BalandinaM <marina.balandina.89@mail.ru>
2026-07-10 23:35:44 +0500

    feat: стилизовать PageNotFound

--- kinopoisk/src/common/components/pageNotFound/PageNotFound.tsx
+++ kinopoisk/src/common/components/pageNotFound/PageNotFound.tsx
@@ -1,10 +1,19 @@
+import { Path } from '@/common/constants'
 import s from './PageNotFound.module.css'
+import { Link } from 'react-router-dom'
 
 export const PageNotFound = () => {
   return (
-    <>
+    <div className={s.wrap}>
       <h1 className={s.title}>404</h1>
-      <h2 className={s.subtitle}>page not found</h2>
-    </>
+      <h2 className={s.subtitle}>Страница не найдена</h2>
+      <p className={s.description}>
+        Похоже, вы попали на несуществующую страницу. Проверьте адрес или
+        вернитесь на главную.
+      </p>
+      <Link to={Path.Main} className={s.button}>
+        На главную
+      </Link>
+    </div>
   )
 }
